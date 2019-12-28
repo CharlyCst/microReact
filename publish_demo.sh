94d7ebb1c6903dc992f548966964be0b121123b3
CharlyCst <charly.castes@epfl.ch>
2019-12-28 19:55:32 +0100

    Configure demo webpage

---
+++ publish_demo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Little trick to publish the demo folder as a github page instead of the root
+git subtree push --prefix demo origin gh-pages
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,52 @@
-module.exports = {
-  mode: "development",
+const HtmlWebpackInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-  entry: "./src/index.tsx",
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx"]
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
+module.exports = env => {
+  console.log(env);
+  // Add inline plugging in production mode
+  const plugins =
+    env && env.production
+      ? [
+          new HtmlWebpackPlugin({
+            inlineSource: ".(ts|tsx|jsx|js|css)$"
+          }),
+          new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
         ]
-      },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
-  }
+      : [];
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  //   externals: {
-  //     react: "React",
-  //     "react-dom": "ReactDOM"
-  //   }
+  return {
+    mode: "development",
+
+    entry: "./src/index.tsx",
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx"]
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader"
+            }
+          ]
+        },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ]
+    },
+
+    plugins: plugins
+  };
 };
